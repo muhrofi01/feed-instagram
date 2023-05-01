2479fe5e1ee8f8f7e45f65877617e7eb520a744c
muhrofi <muhrofi01@gmail.com>
2023-05-01 09:03:10 +0700

    Membuat component simple navbar

---
+++ Frontend/Mei/navbar/script.js
@@ -0,0 +1,20 @@
+const navToggleButton = document.querySelector("button.nav-toggle");
+const navigationOnSidebar = document.querySelector("div.nav");
+const backgroundSidebar = document.querySelector(".background-sidebar");
+
+window.onclick = (event) => {
+  if (event.target === backgroundSidebar) {
+    navigationOnSidebar.classList.remove("slide");
+    backgroundSidebar.style.display = "none";
+    navToggleButton.classList.remove("active");
+  }
+};
+
+navToggleButton.addEventListener("click", () => {
+  navigationOnSidebar.classList.toggle("slide");
+  if (navigationOnSidebar.classList.contains("slide"))
+    backgroundSidebar.style.display = "block";
+  else backgroundSidebar.style.display = "none";
+
+  navToggleButton.classList.toggle("active");
+});
